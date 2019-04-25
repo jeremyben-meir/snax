94907f13af5f126c05aad542a00a2eaa7eaadb70
Jeremy Ben-Meir <jsb459@cornell.edu>
2019-04-25 15:03:18 -0400

    fat button added

--- snax/ViewController.swift
+++ snax/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // ADDED FAT BUTTON
     var filterCollection: UICollectionView!
     var restaurantCollection: UICollectionView!
     var filterArray: [Filter]! = []
