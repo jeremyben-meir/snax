3b4d696b5257f1032a20b683cb55af5776c3a5d1
Jeremy Ben-Meir <jsb459@cornell.edu>
2019-05-05 14:53:53 -0400

    msg

--- snax/Controllers/SignInViewController.swift
+++ snax/Controllers/SignInViewController.swift
@@ -3,14 +3,9 @@ import GoogleSignIn
 
 class SignInViewController: UIViewController, GIDSignInUIDelegate {
     
-    // MARK: - View vars
-    var appNameLabel: UILabel!
     var signInButton: GIDSignInButton!
-    let snaxcolor = UIColor(red: 31/255.0, green: 207/255.0, blue: 131/255.0, alpha: 1.0)
-    
-    // MARK: - Constants
+    var imageView: UIImageView!
     let verticalSpace: CGFloat = 125
-    let appName = "Snax"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,39 +13,29 @@ class SignInViewController: UIViewController, GIDSignInUIDelegate {
         view.backgroundColor = .white
         GIDSignIn.sharedInstance().uiDelegate = self
         
-        setupViews()
-        setupConstraints()
-        
-    }
-    
-    func setupViews() {
-        appNameLabel = UILabel()
-        appNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        appNameLabel.font = UIFont.boldSystemFont(ofSize: 40)
-        appNameLabel.textAlignment = .center
-        appNameLabel.text = appName
-        appNameLabel.textColor = snaxcolor
-        view.addSubview(appNameLabel)
+        imageView = UIImageView(frame: .zero)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "generic_icon")
+        view.addSubview(imageView)
         
         signInButton = GIDSignInButton()
         signInButton.style = .wide
         signInButton.colorScheme = .light
         signInButton.center = view.center
         view.addSubview(signInButton)
+        
+        setupConstraints()
+        
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            appNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            appNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
-            appNameLabel.bottomAnchor.constraint(equalTo: signInButton.topAnchor, constant: -10),
-            appNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor),
-           
-            
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            imageView.bottomAnchor.constraint(equalTo: signInButton.topAnchor, constant: -10),
+            imageView.widthAnchor.constraint(equalTo: view.widthAnchor)
             ])
-}
-    @objc func PresentSignUpScreen(){
-        let signUpViewController = SignUpViewController()
-        present(signUpViewController, animated: true, completion: nil)
     }
 }
